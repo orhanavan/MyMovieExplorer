9b66918c36824c99e8f7688cb67a0461faebf08c
orhanavan <o.avan.71@gmail.com>
2023-11-28 23:55:27 +0300

    Default dark theme added

--- app/src/main/java/com/orhanavan/mymovieexplorer/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/orhanavan/mymovieexplorer/presentation/activity/MainActivity.kt
@@ -8,10 +8,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 }
